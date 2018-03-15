1d71f845878114251a1e5c424de64e19dbb61a3d
Jorge Andrade <j.andrade.dev@gmail.com>
2018-03-15 00:50:55 -0600

    fix style ci issues

--- src/TaxManager.php
+++ src/TaxManager.php
@@ -91,8 +91,6 @@ class TaxManager
                 'amount' => $tax->calculate($this->amount),
             ];
         }, $this->taxes);
-
-
         return array_merge([
             'amount' => $this->amount,
             'total' => $this->total,
